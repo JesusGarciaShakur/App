e33b7f5f5d0e9799de310a221e1bc2de8164f1e6
JesusGarciaShakur <jesusrguez970@gmail.com>
2023-12-10 11:19:50 -0600

    03 Avances theme ui

--- app/src/main/java/com/example/myapplication/ui/AppComponents.kt
+++ app/src/main/java/com/example/myapplication/ui/AppComponents.kt
@@ -1,10 +1,23 @@
 package com.example.myapplication.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -16,14 +29,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
+
 
 @Composable
 fun TopBar(value:String){
@@ -74,6 +90,7 @@ fun TextFieldComponent(
     var currentValue by remember{
         mutableStateOf("")
     }
+    val localFocusModifier = LocalFocusManager.current
     OutlinedTextField(
         modifier = Modifier.fillMaxWidth(),
         value = currentValue,
@@ -84,5 +101,75 @@ fun TextFieldComponent(
         placeholder = {
             Text(text = "Enter your name", fontSize = 18.sp)
         },
-        textStyle = TextStyle.Default.copy(fontSize = 24.sp))
+        textStyle = TextStyle.Default.copy(fontSize = 24.sp),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions {
+            localFocusModifier.clearFocus()
+
+        }
+    )
+
+}
+
+@Composable
+fun AnimalCard(
+    image: Int,
+    selected: Boolean,
+    animalSelected: (animalName: String) -> Unit
+) {
+    val localFocusModifier = LocalFocusManager.current
+
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier
+            .padding(24.dp)
+            .size(130.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .border(
+                    width = 1.dp,
+                    color = if (selected) Color.Green else Color.Transparent,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .clickable {
+                    val animalName = if (image == R.drawable.gato) "Cat" else "Dog"
+                    animalSelected(animalName)
+                    localFocusModifier.clearFocus()
+                }
+        ) {
+            Image(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .wrapContentWidth()
+                    .wrapContentHeight(),
+                painter = painterResource(id = image),
+                contentDescription = "Animal Image"
+            )
+        }
+    }
+}
+
+
+
+
+@Composable
+fun ButtonComponent(
+    goToDetailsScreen: () -> Unit
+){
+    Button(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = {
+            goToDetailsScreen()
+        }) {
+        TextComponent(textValue = "Go to Details Screen"
+            , textSize = 18.sp,
+            colorValue = Color.White
+        )
+
+    }
 }
